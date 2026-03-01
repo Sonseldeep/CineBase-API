2cfc0400eb0c3859a8fa6d1913da6526dfb2a37b
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-03-01 13:14:55 +0545

    feat: inject DB connection and connection string config

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,7 +1,9 @@
+using Movies.Application.Database;
 using Movies.Application.Extensions;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 
 builder.Services.AddControllers();
@@ -9,6 +11,8 @@ builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplication();
+builder.Services.AddDatabase(config["Database:ConnectionString"]!);
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -24,5 +28,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+var dbInitializer = app.Services.GetRequiredService<DbInitializer>();
+await dbInitializer.InitializeAsync();
 
 app.Run();
