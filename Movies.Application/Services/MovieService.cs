52e4b61d3447956f6028f5e6f77c68913307ea7d
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-03-01 16:46:48 +0545

    implemented IMovieService business logic

--- Movies.Application/Services/MovieService.cs
+++ Movies.Application/Services/MovieService.cs
@@ -1,6 +1,44 @@
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+
 namespace Movies.Application.Services;
 
-public class MovieServices
+public class MovieService(IMoviesRepository moviesRepository) : IMovieService
 {
-    
+    public Task<bool> CreateAsync(Movie movie)
+    {
+        return  moviesRepository.CreateAsync(movie);
+    }
+
+    public Task<Movie?> GetByIdAsync(Guid id)
+    {
+        return  moviesRepository.GetByIdAsync(id);
+    }
+
+    public Task<Movie?> GetBySlugAsync(string slug)
+    {
+        return  moviesRepository.GetBySlugAsync(slug);
+    }
+
+    public Task<IEnumerable<Movie>> GetAllAsync()
+    {
+        return  moviesRepository.GetAllAsync();
+    }
+
+    public async Task<Movie?> UpdateAsync(Movie movie)
+    {
+        var movieExists = await moviesRepository.ExistsByIdAsync(movie.Id);
+        if (!movieExists)
+        {
+            return null;
+        }
+
+        await moviesRepository.UpdateAsync(movie);
+        return movie;
+    }
+
+    public Task<bool> DeleteByIdAsync(Guid id)
+    {
+        return  moviesRepository.DeleteByIdAsync(id);
+    }
 }
