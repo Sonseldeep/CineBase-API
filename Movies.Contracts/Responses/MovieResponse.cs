a35f81fd5c5630dc5689dfd9d1e81f5f2fbaf269
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-03-01 16:44:37 +0545

    update Id type into GUID

--- Movies.Contracts/Responses/MovieResponse.cs
+++ Movies.Contracts/Responses/MovieResponse.cs
@@ -2,7 +2,7 @@ namespace Movies.Contracts.Responses;
 
 public class MovieResponse
 {
-    public required string Id { get; init; }
+    public required Guid Id { get; init; }
     
     public required string Slug { get; init; }
     public required string Title { get; init; }
