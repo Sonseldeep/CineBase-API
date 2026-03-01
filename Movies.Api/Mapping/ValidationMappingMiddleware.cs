de3c9cb313d6081df3434160f8ddba5b76271767
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-03-01 19:02:40 +0545

    add middleware for error catching

--- Movies.Api/Mapping/ValidationMappingMiddleware.cs
+++ Movies.Api/Mapping/ValidationMappingMiddleware.cs
@@ -1,6 +1,39 @@
+using FluentValidation;
+using Movies.Contracts.Responses;
+
 namespace Movies.Api.Mapping;
 
 public class ValidationMappingMiddleware
 {
-    
+    private readonly RequestDelegate _next;
+
+    public ValidationMappingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        // ValidationException is thrown by FluentValidation
+        catch (ValidationException ex)
+        { 
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Response.ContentType = "application/json";
+            
+            var validationFailureResponse = new ValidationFailureResponse
+            {
+                Errors = ex.Errors.Select(x =>
+                    new ValidationResponse
+                    {
+                        PropertyName = x.PropertyName,
+                        Message = x.ErrorMessage
+                    })
+            };
+            await context.Response.WriteAsJsonAsync(validationFailureResponse);
+        }
+    }
 }
