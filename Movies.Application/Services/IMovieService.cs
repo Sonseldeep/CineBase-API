4dfc9d51bb0c65abcc9826c47067d54ae3a0a2e0
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-03-01 16:47:27 +0545

    defined the movies services interfaces

--- Movies.Application/Services/IMovieService.cs
+++ Movies.Application/Services/IMovieService.cs
@@ -1,6 +1,15 @@
+using Movies.Application.Models;
+
 namespace Movies.Application.Services;
 
-public class IMovieService
+public interface IMovieService
 {
-    
+    Task<bool> CreateAsync(Movie movie);
+    Task<Movie?> GetByIdAsync(Guid id);
+    Task<Movie?> GetBySlugAsync(string slug);
+    Task<IEnumerable<Movie>> GetAllAsync();
+    // here this is different from the repository because we want to return the updated movie after update
+    Task<Movie?> UpdateAsync(Movie movie);
+    Task<bool> DeleteByIdAsync(Guid id);
+  
 }
