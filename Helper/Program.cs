c33e58436e485675d6f46d0ecd8032c4b8d20a76
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-03-02 11:12:57 +0545

    feat: add audience and issuer

---
+++ Helper/Program.cs
@@ -0,0 +1,26 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+    app.UseSwaggerUI();
+    app.UseSwagger();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
