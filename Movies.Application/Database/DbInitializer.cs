9d9b648c82407919b184b5976e3695b6f0dc6b15
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-03-04 11:07:31 +0545

    feat(rating): add rating table to store movie ratings

--- Movies.Application/Database/DbInitializer.cs
+++ Movies.Application/Database/DbInitializer.cs
@@ -32,6 +32,15 @@ public class DbInitializer(IDbConnectionFactory dbConnectionFactory)
                                            name TEXT not null
                                        ); 
                                        """);
+        
+        await connection.ExecuteAsync($"""
+                                       create table if not exists ratings (
+                                           userid UUID,
+                                           movieid UUID references movies (id),
+                                           rating integer not null,
+                                           primary key (userid, movieid)
+                                       ); 
+                                       """);
     }
 
 }
