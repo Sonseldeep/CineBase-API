646ad52db011e497349d1e7951abb0e2c14c0c51
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-03-04 11:05:37 +0545

    feat(rating): add routes for rating movies

--- Movies.Api/ApiEndpoints/ApiEndpoints.cs
+++ Movies.Api/ApiEndpoints/ApiEndpoints.cs
@@ -13,5 +13,21 @@ public static class ApiEndpoints
         public const string GetAll = Base;
         public const string Update = $"{Base}/{{id:guid}}";
         public const string Delete = $"{Base}/{{id:guid}}";
+        
+        // where are rating going to lives?
+        // rating are directly related to movies
+        
+        public const string Rate = $"{Base}/{{id:guid}}/ratings";
+        public const string DeleteRating = $"{Base}/{{id:guid}}/ratings";
+        
+    }
+
+    // get all the rating for user
+
+    public static class Ratings
+    {
+        private const string Base = $"{ApiBase}/ratings";
+
+        public const string GetUserRatings = $"{Base}/me";
     }
 }
