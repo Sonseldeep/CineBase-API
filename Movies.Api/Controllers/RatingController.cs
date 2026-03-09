c12c5dedc3df7cf7244432a8b8f83ae84bda2a63
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-03-09 12:57:37 +0545

    add user can get own rating in controller

--- Movies.Api/Controllers/RatingController.cs
+++ Movies.Api/Controllers/RatingController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Movies.Api.Auth.Extensions;
+using Movies.Api.Mapping;
 using Movies.Application.Services;
 using Movies.Contracts.Requests;
 
@@ -25,6 +26,27 @@ public class RatingController : ControllerBase
         var result = await _ratingService.RateMovieAsync(id, request.Rating, userId!.Value, token);
 
         return result ? Ok() : NotFound();
+    }
+
+    [Authorize]
+    [HttpDelete(ApiEndpoints.ApiEndpoints.Movies.DeleteRating)]
+    public async Task<IActionResult> DeleteRating([FromRoute] Guid id,CancellationToken token)
+    {
+        var userId = HttpContext.GetUserId();
+        var result = await _ratingService.DeleteRatingAsync(id, userId!.Value, token);
+        
+        return result ? NoContent(): NotFound();
+    }
+
+    [Authorize]
+    [HttpGet(ApiEndpoints.ApiEndpoints.Ratings.GetUserRatings)]
+    public async Task<IActionResult> GetUserRatings(CancellationToken token = default)
+    {
+        var userId = HttpContext.GetUserId();
+        var ratings = await _ratingService.GetRatingsForUserAsync(userId!.Value, token);
+        var ratingsResponse = ratings.MapToResponse();
+        
+        return Ok(ratingsResponse);
     }
     
 }
