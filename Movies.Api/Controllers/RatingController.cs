371fbc139dcadb0703291412f1b2702c507b3157
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-03-09 12:23:56 +0545

    fear(rating): add rating movie controller

--- Movies.Api/Controllers/RatingController.cs
+++ Movies.Api/Controllers/RatingController.cs
@@ -1,6 +1,30 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Auth.Extensions;
+using Movies.Application.Services;
+using Movies.Contracts.Requests;
+
 namespace Movies.Api.Controllers;
 
-public class RatingController
+[ApiController]
+public class RatingController : ControllerBase
 {
+    private readonly IRatingService _ratingService;
+
+    public RatingController(IRatingService ratingService)
+    {
+        _ratingService = ratingService;
+    }
+
+    [Authorize]
+    [HttpPut(ApiEndpoints.ApiEndpoints.Movies.Rate)]
+    public async Task<IActionResult> RateMovie([FromRoute] Guid id,
+        [FromBody] RateMovieRequest request, CancellationToken token)
+    {
+        var userId = HttpContext.GetUserId();
+        var result = await _ratingService.RateMovieAsync(id, request.Rating, userId!.Value, token);
+
+        return result ? Ok() : NotFound();
+    }
     
 }
